cef6e251a8ed3b8c2e75a5a94bde8a5d99258caa
myname21-meet <myname21@meet.mit.edu>
2020-01-30 10:10:55 -0500

    kkk

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-from passlib.apps import custom_app_context as pwd_security
 Base= declarative_base()
 
 class User(Base):
